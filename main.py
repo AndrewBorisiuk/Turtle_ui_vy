34f7c6b730d7700bd3674efa4d4690a7c27470bc
AndrewBorisiuk <dmitriy.borysiuk@gmail.com>
2026-08-18 19:18:01 +0300

    add_flower_functionality

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 print("Яку гру ви бажаєте зіграти?")
 print("1 - Хрестики-нулики")
 print("2 - Вікторина слова")
+print("3 - Квітка")
 
-choice = input("Введіть 1 або 2: ")
+choice = input("Введіть 1, 2 або 3: ")
 
 if choice == "1":
     import cross
 elif choice == "2":
     import guess
+elif choice == "3":
+    import flower
 else:
-    print("Пеперошуємо, будь ласка, введіть 1 або 2")
+    print("Пеперошуємо, будь ласка, введіть 1, 2 або 3")
